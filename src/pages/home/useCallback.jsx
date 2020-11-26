4dedca67d50ecc70c2357080897553196973395d
hanyan <bianfj@acloudglobal.com>
2020-11-26 09:42:51 +0800

    加注释

--- src/pages/home/useCallback.jsx
+++ src/pages/home/useCallback.jsx
@@ -6,6 +6,12 @@
 父组件更新会得到新的handleOnChange，传递给子组件，对于子组件来说接收到一个新的props
 子组件进行不必要更新
 
+
+handleOnChange被缓存了下来，尽管父组件更新了，但是拿到的handleOnChange还是同一个
+对比useMemo，useMemo缓存的是一个值，useCallback缓存的是一个函数，是对一个单独的props值进行缓存
+memo缓存的是组件本身，是站在全局的角度进行优化
+
+
  * @Author: 寒嫣
  * @Date: 2020-11-25 18:19:57
  */
